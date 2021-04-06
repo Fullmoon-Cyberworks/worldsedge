33d66f584a7f04acd4114a8fcaf59715fe3bc2e6
OGTRD <monfortjeffery@gmail.com>
2021-04-06 11:12:07 -0500

    Added a floor tile to mess with collisions, started a auto tile
    Added a dirt path placeholder tile, spaced out the movement code to read it better. Started an auto tile though it's not usable yet.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export(int) var speed = 5
+export(int) var speed = 500
 var motion = Vector2()
 
 func cart_to_iso(cartesian):#changes cartersian coordinates to an isometric functioning coordinate system
@@ -12,33 +12,43 @@ func _physics_process(delta): #Handles Movements, Direction, and All sprite anim
 	if Input.is_action_pressed("ui_right"):
 		direction += Vector2(1,0)
 		$AnimatedSprite.play("1_Run")
+		
 	elif Input.is_action_just_released("ui_right"):
 			$AnimatedSprite.play("1_Idle")
+			
 	if Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_down"):
 		direction += Vector2(1,1)
 		$AnimatedSprite.play("2_Run")
+		
 	if Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_up"):
 		direction += Vector2(1,-1)
 		$AnimatedSprite.play("0_Run")
 	if Input.is_action_pressed("ui_down"):
 		direction += Vector2(0,1)
 		$AnimatedSprite.play("3_Run")
+		
 	elif Input.is_action_just_released("ui_down"):
 			$AnimatedSprite.play("3_Idle")
+			
 	if Input.is_action_pressed("ui_left"):
 		direction += Vector2(-1,0)
 		$AnimatedSprite.play("5_Run")
+		
 	if Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_down"):
 		direction += Vector2(-1,1)
 		$AnimatedSprite.play("4_Run")
+		
 	if Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_up"):
 		direction += Vector2(-1,-1)
 		$AnimatedSprite.play("6_Run")
+		
 	elif Input.is_action_just_released("ui_left"):
 			$AnimatedSprite.play("5_Idle")
+			
 	if Input.is_action_pressed("ui_up"):
 		direction += Vector2(0,-1)
 		$AnimatedSprite.play("7_Run")
+		
 	elif Input.is_action_just_released("ui_up"):
 			$AnimatedSprite.play("7_Idle")
 	
